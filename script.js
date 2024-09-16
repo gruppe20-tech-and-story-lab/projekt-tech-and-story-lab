106acb8376fc945ddcb98ecc1a3c10f2c7593cb5
Melina Pannach <meth0001@stud.kea.dk>
2024-09-16 17:02:46 +0200

    js/v1

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 //https://uihgaqtyehbbzlxkyijf.supabase.co
 //eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVpaGdhcXR5ZWhiYnpseGt5aWpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5NjU1NjIsImV4cCI6MjA0MTU0MTU2Mn0.aSQ02uWSHbNbarVsY3v4WZZu6TJqc8ALMnhz32Lb3jA
-fetch("")
+fetch("https://uihgaqtyehbbzlxkyijf.supabase.co/rest/v1/tsl-assets", {
+    method:"GET",
+    headers: {
+        apikey:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVpaGdhcXR5ZWhiYnpseGt5aWpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5NjU1NjIsImV4cCI6MjA0MTU0MTU2Mn0.aSQ02uWSHbNbarVsY3v4WZZu6TJqc8ALMnhz32Lb3jA"
+    }
+})
 .then(res=>res.json())
 .then(showData)
 
 function showData(items){
-items.forEach()
+    console.log(items);
+////items.forEach()
 
 }
 
