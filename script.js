c64c16d69beb297c32b0338e5a28d180a87047d9
Melina Pannach <meth0001@stud.kea.dk>
2024-09-16 16:51:49 +0200

    js

---
+++ script.js
@@ -0,0 +1,12 @@
+//https://uihgaqtyehbbzlxkyijf.supabase.co
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVpaGdhcXR5ZWhiYnpseGt5aWpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU5NjU1NjIsImV4cCI6MjA0MTU0MTU2Mn0.aSQ02uWSHbNbarVsY3v4WZZu6TJqc8ALMnhz32Lb3jA
+fetch("")
+.then(res=>res.json())
+.then(showData)
+
+function showData(items){
+items.forEach()
+
+}
+
+function(){}
